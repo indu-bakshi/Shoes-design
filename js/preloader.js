f98242459de6fd106b0c71fff97bc5901483c44b
Indu Bakshi <bakshi.webworld@gmail.com>
2020-07-25 19:26:41 +0530

    Add files via upload
    Commit 1

---
+++ js/preloader.js
@@ -0,0 +1,33 @@
+TweenMax.to("#first_logo",2,{
+    delay: 1,
+    y: -500,
+    ease: Expo.easeInOut,
+    opacity:0
+});
+
+TweenMax.to(".overlay",2,{
+    delay: 2,
+    top: "-100%",
+    ease: Expo.easeInOut
+  });
+TweenMax.from(".nav",2,{
+    delay: 2.5,
+    x:-200,
+    ease: Expo.easeInOut
+  });
+  TweenMax.from(".text",2,{
+    delay: 2.8,
+    x:600,
+    ease: Expo.easeInOut
+  });
+  TweenMax.from(".colors",3,{
+    delay: 2.8,
+    x:500,
+    ease: Expo.easeInOut
+  });
+  TweenMax.from(".circle-rotate",4,{
+    delay: 3.5,
+    opacity:0,
+    ease: Expo.easeInOut
+  });
+
